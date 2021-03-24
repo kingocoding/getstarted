2cb8d009a5e2e8398fc4bd12ea27a9a9468ba470
Charlie <myforest@gmail.com>
2021-03-25 00:03:25 +0900

    part 3 commit

--- game.js
+++ game.js
@@ -1,7 +1,8 @@
 var config = {
-    width: 800,
-    height: 600,
+    width: 256,
+    height: 272,
     backgroundColor: 0x000000,
-    scene: [Scene1, Scene2]
+    scene: [Scene1, Scene2],
+    pixelArt: true
 }
 var game=new Phaser.Game(config);
