8c532b1c589e0f4891cc15205e93dd298873e81e
Charlie <myforest@gmail.com>
2021-03-26 13:46:37 +0900

    part 6 physics

--- Scene1.js
+++ Scene1.js
@@ -5,11 +5,7 @@ class Scene1 extends Phaser.Scene {
   
     preload(){
       this.load.image("background", "assets/images/background.png");
-      // 0 chnage the path to spritesheets
-      // this.load.image("ship", "assets/images/ship.png");
-      // this.load.image("ship2", "assets/images/ship2.png");
-      // this.load.image("ship3", "assets/images/ship3.png");
-      // to
+      // 
       this.load.spritesheet("ship", "assets/spritesheets/ship.png",{
         frameWidth: 16,
         frameHeight: 16
@@ -26,6 +22,12 @@ class Scene1 extends Phaser.Scene {
         frameWidth: 16,
         frameHeight: 16
       });
+
+      this.load.spritesheet("power-up", "assets/spritesheets/power-up.png", {
+        frameWidth: 16,
+        frameHeight: 16
+      })
+      
     }
   
     create() {
